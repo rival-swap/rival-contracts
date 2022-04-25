dc092bb26532d5d6457905c2b94c824a5567ca92
Literally Collective <bluedragon.cstar@outlook.com>
2022-04-25 02:39:04 -0300

    Create RewardDistributor.sol

---
+++ contracts/RewardDistributor.sol
@@ -0,0 +1,89 @@
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+import "./libs/IBEP20.sol";
+
+// Reward distributor contract for the masterchef
+contract RewardDistributor is Ownable, ReentrancyGuard {
+    // The $RIVAL TOKEN!
+    IBEP20 public rivalToken;
+
+    // The operator can only withdraw wrong tokens in the contract
+    address private _operator;
+
+    // Event
+    event OperatorTransferred(
+        address indexed previousOperator,
+        address indexed newOperator
+    );
+    event OperatorTokenRecovery(address tokenRecovered, uint256 amount);
+
+    modifier onlyOperator() {
+        require(
+            _operator == _msgSender(),
+            "operator: caller is not the operator"
+        );
+        _;
+    }
+
+    constructor(IBEP20 _rivalToken) public {
+        rivalToken = _rivalToken;
+        _operator = _msgSender();
+
+        emit OperatorTransferred(address(0), _operator);
+    }
+
+    // Safe $RIVAL transfer function, just in case if rounding error causes pool to not have enough $RIVALs.
+    function safeRivalTokenTransfer(address _to, uint256 _amount)
+        external
+        onlyOperator
+        nonReentrant
+        returns (uint256)
+    {
+        uint256 rivalBal = rivalToken.balanceOf(address(this));
+        if (_amount > rivalBal) {
+            _amount = rivalBal;
+        }
+        if (_amount > 0) {
+            rivalToken.transfer(_to, _amount);
+            return _amount;
+        }
+        return 0;
+    }
+
+    /**
+     * @dev operator of the contract
+     */
+    function operator() external view returns (address) {
+        return _operator;
+    }
+
+    /**
+     * @dev Transfers operator of the contract to a new account (`newOperator`).
+     * Can only be called by the current operator.
+     */
+    function transferOperator(address newOperator) external onlyOwner {
+        require(
+            newOperator != address(0),
+            "RewardDistributor::transferOperator: new operator is the zero address"
+        );
+        emit OperatorTransferred(_operator, newOperator);
+        _operator = newOperator;
+    }
+
+    /**
+     * @notice It allows the operator to recover wrong tokens sent to the contract
+     * @param _tokenAddress: the address of the token to withdraw
+     * @param _tokenAmount: the number of tokens to withdraw
+     * @dev This function is only callable by operator.
+     */
+    function recoverWrongTokens(address _tokenAddress, uint256 _tokenAmount)
+        external
+        onlyOwner
+    {
+        IBEP20(_tokenAddress).transfer(_msgSender(), _tokenAmount);
+        emit OperatorTokenRecovery(_tokenAddress, _tokenAmount);
+    }
+}
